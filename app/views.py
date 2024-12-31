bd2260babcf6d3807c75a761c9449db70d52f13d
MaheryAndrih1 <maheryandrianaivo1@gmail.com>
2024-12-31 22:18:59 +0300

    can add goals

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Goal
+from .forms import GoalForm
 
 # Create your views here.
 def home(request):
@@ -10,5 +11,13 @@ def home(request):
     return render(request, 'home.html', context)
 
 def add_goals(request):
-    return render(request, 'add_goals.html')
+    if request.method == "POST":
+        form = GoalForm(request.POST)
+        if form.is_valid:
+            goal=form.save()
+            print('posted')
+    else:
+        form = GoalForm()
+            
+    return render(request, 'add_goals.html', {"form": form})
 
